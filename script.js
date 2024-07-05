a1a69603d563858f630c0c1aee2e24f358aa4a0c
geraz0 <gabbi.erazo1@gmail.com>
2024-07-05 13:18:33 -0400

    fixing api integration

--- script.js
+++
@@ -1,4 +0,0 @@
-//Client-side (JavaScript file connected to HTML)
-//You will have a JavaScript file linked to your HTML page.
-//This JavaScript file will send a request to your Express server (app.js).
-//Once the data is received from the server, this JavaScript file will handle displaying the data on the HTML page
